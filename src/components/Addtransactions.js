b1de28f0c4fc859cbe4902b738731c9446bb5527
nishithagentyala <nishithagentyala@gmail.com>
2022-05-26 19:51:09 +0530

    updated code

--- src/components/Addtransactions.js
+++ src/components/Addtransactions.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import "./TransactionList.css";
 import { GlobalContext } from "../context/GlobalState";
 const Addtransactions = () => {
